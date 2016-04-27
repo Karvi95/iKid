78491217cc7046a23f4ddf3f3d8da23f8d6ff317
Karvi90 <karvi90>
2016-04-27 15:16:03 -0700

    Finished one main ViewController

--- iKid/iKid/ProgrammingViewController.swift
+++ iKid/iKid/ProgrammingViewController.swift
@@ -13,6 +13,9 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        firstBuilder();
+        switchViewController(nil, to: setupViewController)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,7 +23,55 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
+    private var setupViewController : ProgrammingQuestViewController!
+    private var punchlineViewController : ProgrammingAnswerViewController!
 
+    
+    private func firstBuilder() {
+        if setupViewController == nil {
+            setupViewController = storyboard?.instantiateViewControllerWithIdentifier("Programming Setup")
+            as! ProgrammingQuestViewController
+        }
+    }
+    private func secondBuilder() {
+        if punchlineViewController == nil {
+            punchlineViewController = storyboard?.instantiateViewControllerWithIdentifier("Programming Punchline")
+            as! ProgrammingAnswerViewController
+        }
+    }
+    
+    @IBAction func RevealPunchline(sender: UIBarButtonItem) {
+        secondBuilder()
+        firstBuilder()
+        
+        UIView.beginAnimations("View Flip", context: nil)
+        UIView.setAnimationDuration(0.4)
+        UIView.setAnimationCurve(.EaseInOut)
+        
+        if setupViewController != nil && setupViewController?.view.superview != nil {
+            UIView.setAnimationTransition(.FlipFromRight, forView: view, cache: true)
+            punchlineViewController.view.frame = view.frame
+            switchViewController(setupViewController, to: punchlineViewController)
+        } else {
+            UIView.setAnimationTransition(.FlipFromLeft, forView: view, cache: true)
+            setupViewController.view.frame = view.frame
+            switchViewController(punchlineViewController, to: setupViewController)
+        }
+     UIView.commitAnimations()
+    }
+    
+    private func switchViewController(from: UIViewController?, to: UIViewController?) {
+        if from != nil {
+            from!.willMoveToParentViewController(nil)
+            from!.view.removeFromSuperview()
+            from!.removeFromParentViewController()
+        }
+        
+        if to != nil {
+            self.addChildViewController(to!)
+            self.view.insertSubview(to!.view, atIndex: 0)
+            to!.didMoveToParentViewController(self)
+        }
+    }
 }
 
