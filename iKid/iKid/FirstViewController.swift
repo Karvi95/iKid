280089ed0b8f47d28ad376553a3e51025e007684
karvi90 <karvi90@gmail.com>
2016-04-27 13:12:55 -0700

    ugh

--- iKid/iKid/FirstViewController.swift
+++ iKid/iKid/FirstViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FirstViewController.swift
 //  iKid
 //
 //  Created by iGuest on 4/27/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
